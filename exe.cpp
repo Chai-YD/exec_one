3179246bf5a832b42ad4aebba78b07b60c830dd1
Chai-YD <1030333788@qq.com>
2018-07-05 20:39:07 -0400

    7_6_cpp_1

---
+++ exe.cpp
@@ -0,0 +1,50 @@
+//#include<iostream.h> vc6.0依旧支持这个旧的头文件
+//这个是旧C++的头文件 ，新的编译器（如vs2008及以上 ）都不再支持
+
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//    std::cout<<"hello world"<<std::endl;
+//    return 0;
+//}
+
+
+//cout是标准输出流对象，<<是输出操作符
+//cin是标准输入流对象，>>是输入操作符
+//#include<stdio.h>
+//
+//int main(){
+//    int i2 = 1;
+//    double d2 = 2.22;
+//    printf("c type :int->%d,double->%f\n",i2,d2);
+//
+//    printf("please input int and double:");
+//    scanf("%d, %f",&i2,&d2);           //注意输入的操作
+//    printf("c type :int->%d,double->%f\n",i2,d2);
+//    return 0;
+//}
+
+
+//#include<iostream>
+////using namespace std;
+//
+//int main(){
+//    int i2 = 1;
+//    double d2 = 1.1215;
+//    std::cout<<"c++ type:"<<"int->"<<i2<<"double->"<<d2<<std::endl;
+//    std::cout<<"please input int and double:";
+//    std::cin>>i2>>d2;
+//    std::cout<<"c++ type:"<<"int->"<<i2<<"double->"<<d2<<std::endl;
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
