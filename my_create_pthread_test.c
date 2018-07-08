1bd8c165ac3c73d20675e5ea064ec71661423b75
Chai-YD <1030333788@qq.com>
2018-07-07 21:24:00 -0400

    线程的创建

---
+++ my_create_pthread_test.c
@@ -0,0 +1,30 @@
+//线程的优点
+//   1.创建一个线程的开销成本较低
+//   2.线程的调度需要的操作远少于进程的调度
+//   3.线程占用的资源远少于进程
+//   4.能充分的利用多处理机可并行数量
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void *handler(void* mag){
+    int i;
+    printf("I am thread\n");
+    sleep(1);
+}
+
+//主函数
+int main(){
+    pthread_t tid;
+    int ret ;
+    if((ret = pthread_create(&tid,NULL,handler,NULL)) != 0){
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+    for(;;){
+        printf("I am Main thread\n");
+        sleep(1);
+    }
+    return 0;
+}
